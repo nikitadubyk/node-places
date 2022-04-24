0f897754f81f40f1c5f69e7634883a9ccd69000a
Nikita Dubyk <nikita_dubyk@mail.ru>
2022-04-24 18:31:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Users from './user/pages/Users'
+import NewPlaces from './places/pages/NewPlaces'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Users />} />
+                <Route path='/places/new' element={<NewPlaces />} />
+                <Route path='*' element={<Navigate to='/' replace />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
